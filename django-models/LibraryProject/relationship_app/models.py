69ee81f84f8d50bbe9879dbe37aed5bb7e49e048
Yerosen Bekele <yerobeke@gmail.com>
2025-09-23 12:52:35 +0300

    add

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,3 +5,11 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
