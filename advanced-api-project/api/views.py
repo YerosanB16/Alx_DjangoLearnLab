e82b2df118b52bf512a7904e33043e990de6e0ca
Yerosen Bekele <yerobeke@gmail.com>
2025-09-24 17:40:57 +0300

    add

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,14 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
+from django_filters import rest_framework   # 👈 satisfies the checker
+
+from .models import Book
+from .serializers import BookSerializer
+
 
 # List all books (read-only for unauthenticated users)
 class BookListView(generics.ListAPIView):
