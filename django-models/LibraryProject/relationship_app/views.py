5f760895b6d84acac3af0cd943a9358e5c3c8b9f
Yerosen Bekele <yerobeke@gmail.com>
2025-09-24 16:45:35 +0300

    add

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from .models import Library
-
+from django.views.generic.detail import DetailView
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Author, Book, Library, Librarian, UserProfile  # explicitly include all models
