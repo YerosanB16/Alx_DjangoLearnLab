02330b4d36116ac30f83dac147d7aa5c8fbfcf47
Yerosen Bekele <yerobeke@gmail.com>
2025-09-24 15:59:02 +0300

    add

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Author, Book, Library, Librarian
+from .models import Author, Book, Library, Librarian  # <-- Make sure Library is here
+
 
 # Function-based views
 def home(request):
